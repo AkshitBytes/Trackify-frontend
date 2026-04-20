49fb378d7cb83748a82e721bb217fcb54cb11615
AkshitBytes <akshitKumar129@gmail.com>
2026-04-20 19:09:51 +0530

    changed localhost to deployed url

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://trackify-backend-7si7.onrender.com/api',
 });
 
 // Add a request interceptor
